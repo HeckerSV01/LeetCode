18659f4a79c075aff298aab8d5d77f44c3b9deca
Sumedh Verma <SV839030@GMAIL.COM>
2025-01-11 22:53:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (28.51%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n==0)
+        {
+            return 1;
+        }
+        if(n==-1)
+        {
+            return 1/x;
+        }
+        if(n%2==0)
+        {
+            return myPow(x*x,n/2);
+        }
+        else
+        {
+            return ((n<0)?(1/x):x)*myPow(x*x,n/2);
+        }
+
     }
 }
