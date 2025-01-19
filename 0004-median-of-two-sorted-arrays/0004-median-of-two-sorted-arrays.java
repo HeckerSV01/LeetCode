9ce0b63a092bee5ad34e5ef99d10121179f383ca
Sumedh Verma <SV839030@GMAIL.COM>
2025-01-19 13:24:11 +0530

    Time: 2 ms (59.22%), Space: 46.1 MB (56.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0;
-        int j=0;
-        int k=0;
-        int temp[]=new int[nums1.length+nums2.length];
-        while(i<nums1.length&&j<nums2.length)
-        {
-            if(nums1[i]<=nums2[j])
-            {
-               temp[k]=nums1[i];
-               i++;
-            }
-            else
-            {
-                temp[k]=nums2[j];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int temp[] = new int[nums1.length + nums2.length];
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                temp[k] = nums1[i];
+                i++;
+            } else {
+                temp[k] = nums2[j];
                 j++;
             }
             k++;
         }
-        while(i<nums1.length)
-        {
-            temp[k++]=nums1[i++];
+        while (i < nums1.length) {
+            temp[k++] = nums1[i++];
         }
-        while(j<nums2.length)
-        {
-            temp[k++]=nums2[j++];
+        while (j < nums2.length) {
+            temp[k++] = nums2[j++];
         }
-        if(temp.length%2==0)
-        {
-            return (double)(temp[(temp.length-1)/2]+temp[((temp.length-1)/2)+1])/2;
-        }
-        else
-        {
-            return (double)temp[(temp.length-1)/2];
+        if (temp.length % 2 == 0) {
+            return (double) (temp[(temp.length - 1) / 2] + temp[((temp.length - 1) / 2) + 1]) / 2;
+        } else {
+            return (double) temp[(temp.length - 1) / 2];
         }
     }
 }
