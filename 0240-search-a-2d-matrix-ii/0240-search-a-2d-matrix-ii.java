be0502fd53b619e52f44050abb2e5307203f0363
Sumedh Verma <SV839030@GMAIL.COM>
2025-01-30 19:59:29 +0530

    Time: 5 ms (99.75%), Space: 45.6 MB (93.16%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int c=matrix[0].length-1;
+        int r=0;
+        while(c>=0&&r<matrix.length)
+        {
+            if(matrix[r][c]==target)
+            {
+                return true;
+            }
+            else if(matrix[r][c]>target)
+            {
+                c--;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        return false;
+    }
+}
