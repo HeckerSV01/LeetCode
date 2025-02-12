9a1154462a4bcfa229cdfc968a62cd8fbbd2a64c
Sumedh Verma <SV839030@GMAIL.COM>
2025-02-12 18:31:26 +0530

    Time: 2 ms (89.83%), Space: 44.8 MB (86.86%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,62 @@
+class Solution {
+    public static boolean issafe(char arr[][],int row,int col)
+    {
+        for(int i=row-1;i>=0;i--)
+        {
+            if(arr[i][col]=='Q')
+            {
+                return false;
+            }
+        }
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+        {
+            if(arr[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        for(int i=row-1,j=col+1;i>=0&&j<arr.length;i--,j++)
+        {
+            if(arr[i][j]=='Q')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void solve(List<List<String>> main,char arr[][],int row)
+    {
+       if(row==arr.length)
+       {
+        List<String> list=new ArrayList<>();
+        for(char aux[]:arr)
+        {
+            list.add(new String(aux));
+        }
+        main.add(list);
+        return;
+       }
+       for(int i=0;i<arr.length;i++)
+       {
+        if(issafe(arr,row,i))
+        {
+            arr[row][i]='Q';
+            solve(main,arr,row+1);
+            arr[row][i]='.';
+        }
+       }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> main=new ArrayList<>();
+        char arr[][]=new char[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]='.';
+            }
+        }
+        solve(main,arr,0);
+        return main;
+    }
+}
