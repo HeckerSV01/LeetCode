aaa3ee2fb3165d234d8b8797403f67289367d671
Sumedh Verma <SV839030@GMAIL.COM>
2025-07-23 14:31:11 +0530

    Time: 150 ms (43.84%), Space: 45.5 MB (77.4%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,39 @@
+class Solution {
+    int res=0;
+    public String findandremove(String st,String s,int score)
+    {
+        char x=s.charAt(0);
+        char y=s.charAt(1);
+        Stack<Character> stack=new Stack<>();
+        for(char c:st.toCharArray())
+        {
+            if(!stack.isEmpty()&&stack.peek()==x&&c==y)
+            {
+                stack.pop();
+                res=res+score;
+            }
+            else
+            {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char c:stack)
+        {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    public int maximumGain(String s, int x, int y) {
+        if(x>=y)
+        {
+            s=findandremove(s,"ab",x);
+            s=findandremove(s,"ba",y);
+        }
+        else
+        {
+            s=findandremove(findandremove(s,"ba",y),"ab",x);
+        }
+        return res;
+    }
+}
