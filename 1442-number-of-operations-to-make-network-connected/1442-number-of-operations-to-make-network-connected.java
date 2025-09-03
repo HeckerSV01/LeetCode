0dbb0438e368a1693bd7799af9d5bd1630d500aa
Sumedh Verma <SV839030@GMAIL.COM>
2025-09-03 15:41:34 +0530

    Time: 20 ms (13.01%), Space: 64.4 MB (35.86%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,71 @@
+class disjointSet{
+    public List<Integer> parent=new ArrayList<>();
+    public List<Integer> size=new ArrayList<>();
+    public disjointSet(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    public int find(int node)
+    {
+        if(node==parent.get(node))
+        {
+            return node;
+        }
+        int ultparent=find(parent.get(node));
+        parent.set(node,ultparent);
+        return parent.get(node);
+    }
+    public void unionbyrank(int u,int v)
+    {
+        int ultofu=find(u);
+        int ultofv=find(v);
+        if(ultofu==ultofv)
+        {
+            return;
+        }
+        if(size.get(ultofu)<size.get(ultofv))
+        {
+            parent.set(ultofu,ultofv);
+            size.set(ultofv,size.get(ultofv)+size.get(ultofu));
+        }
+        else
+        {
+            parent.set(ultofv,ultofu);
+            size.set(ultofu,size.get(ultofu)+size.get(ultofv));
+        }
+    }
+}
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        disjointSet ds=new disjointSet(n);
+        int edges=0;
+        for(int i=0;i<connections.length;i++)
+        {
+            if(ds.find(connections[i][0])==ds.find(connections[i][1]))
+            {
+                edges++;
+            }
+            else
+            {
+                ds.unionbyrank(connections[i][0],connections[i][1]);
+            }
+        }
+        int compo=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.find(i)==i)
+            {
+                compo++;
+            }
+        }
+        if(edges>=compo-1)
+        {
+            return compo-1;
+        }
+        return -1;
+    }
+}
