fd71674703ce29d44c165f985ccd96708addc7a2
Sumedh Verma <SV839030@GMAIL.COM>
2025-10-03 22:11:56 +0530

    Time: 53 ms (33.87%), Space: 51.1 MB (45.27%) - LeetHub

---
+++ 1582-design-browser-history/1582-design-browser-history.java
@@ -0,0 +1,50 @@
+class ListNode{
+    String val;
+    ListNode prev;
+    ListNode next;
+    public ListNode(String val)
+    {
+        this.val=val;
+        next=null;
+        prev=null;
+    }
+}
+class BrowserHistory {
+ListNode head;
+    public BrowserHistory(String homepage) {
+        head=new ListNode(homepage);
+    }
+    
+    public void visit(String url) {
+        ListNode curr=new ListNode(url);
+        head.next=curr;
+        curr.prev=head;
+        head=curr;
+    }
+    
+    public String back(int steps) {
+        while(head.prev!=null&&steps>0)
+        {
+            head=head.prev;
+            steps--;
+        }
+        return head.val;
+    }
+    
+    public String forward(int steps) {
+        while(head.next!=null&&steps>0)
+        {
+            head=head.next;
+            steps--;
+        }
+        return head.val;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
