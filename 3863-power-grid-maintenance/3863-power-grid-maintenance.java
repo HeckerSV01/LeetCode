0608bfbaba8b2e45020d0ca54090128fbcb576c8
Sumedh Verma <SV839030@GMAIL.COM>
2025-10-23 20:32:39 +0530

    Time: 162 ms (28.96%), Space: 137.6 MB (83.01%) - LeetHub

---
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.java
@@ -0,0 +1,110 @@
+class disjointSet{
+    public List<Integer> parent=new ArrayList<>();
+    public List<Integer> size=new ArrayList<>();
+    public disjointSet(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    public int find(int node)
+    {
+        if(node==parent.get(node))
+        {
+            return node;
+        }
+        int ultparent=find(parent.get(node));
+        parent.set(node,ultparent);
+        return parent.get(node);
+    }
+    public void unionbysize(int u,int v)
+    {
+        int ultofu=find(u);
+        int ultofv=find(v);
+        if(ultofu==ultofv)
+        {
+            return;
+        }
+        if(size.get(ultofu)<size.get(ultofv))
+        {
+            parent.set(ultofu,ultofv);
+            size.set(ultofv,size.get(ultofv)+size.get(ultofu));
+        }
+        else
+        {
+            parent.set(ultofv,ultofu);
+            size.set(ultofu,size.get(ultofu)+size.get(ultofv));
+        }
+    }
+}
+class Solution {
+    public int getmin(TreeSet<Integer> set,disjointSet ds,int n)
+    {
+        for(int k:set)
+        {
+            if(ds.find(k)==ds.find(n))
+            {
+                return k;
+            }
+        }
+        return -1;
+    }
+    public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        boolean status[]=new boolean[c+1];
+        Arrays.fill(status,true);
+        disjointSet ds=new disjointSet(c+1);
+        for(int k[]:connections)
+        {
+            ds.unionbysize(k[0],k[1]);
+        }
+        HashMap<Integer,TreeSet<Integer>> map=new HashMap<>();
+        for(int i=1;i<=c;i++)
+        {
+            int par=ds.find(i);
+            if(!map.containsKey(par))
+            {
+                map.put(par,new TreeSet<>());
+            }
+            map.get(par).add(i);
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int k[]:queries)
+        {
+            if(k[0]==1)
+            {
+                if(status[k[1]])
+                {
+                    list.add(k[1]);
+                }
+                else
+                {
+                    int par=ds.find(k[1]);
+                    TreeSet<Integer> set=map.get(par);
+                    if(set==null||set.isEmpty())
+                    {
+                        list.add(-1);
+                    }
+                    else
+                    {
+                        list.add(set.first());
+                    }
+                }
+            }
+            else 
+            {
+                status[k[1]]=false;
+                int par=ds.find(k[1]);
+                map.get(par).remove(k[1]);
+            }
+        }
+        int res[]=new int[list.size()];
+        int i=0;
+        for(int k:list)
+        {
+            res[i++]=k;
+        }
+        return res;
+    }
+}
