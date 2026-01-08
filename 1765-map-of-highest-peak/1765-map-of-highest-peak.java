bb24810c281452683b9e1061183030346b6aaf1b
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-08 22:38:43 +0530

    Time: 64 ms (9.52%), Space: 191.1 MB (5.1%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -2,6 +2,7 @@ class info {
     int i;
     int j;
     int ht;
+
     public info(int i, int j, int ht) {
         this.i = i;
         this.j = j;
@@ -20,24 +21,24 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (isWater[i][j] == 1) {
                     q.add(new info(i, j, 0));
-                    res[i][j]=0;
+                    res[i][j] = 0;
                     vis[i][j] = 1;
                 }
             }
         }
         while (!q.isEmpty()) {
             info k = q.remove();
-            int i=k.i;
-            int j=k.j;
-            int curh=k.ht;
-            int dir[][]={{-1, 0, 1, 0}, {0, 1, 0, -1}};
-            for (int l=0;l<4;l++) {
-                int row=i+dir[0][l];
-                int col=j+dir[1][l];
-                if(row>=0&&row<m&&col>=0&&col<n&&vis[row][col]==0){
-                    vis[row][col]=1;
-                    q.add(new info(row, col, curh+1));
-                    res[row][col]=curh+1;
+            int i = k.i;
+            int j = k.j;
+            int curh = k.ht;
+            int dir[][] = { { -1, 0, 1, 0 }, { 0, 1, 0, -1 } };
+            for (int l = 0; l < 4; l++) {
+                int row = i + dir[0][l];
+                int col = j + dir[1][l];
+                if (row >= 0 && row < m && col >= 0 && col < n && vis[row][col] == 0) {
+                    vis[row][col] = 1;
+                    q.add(new info(row, col, curh + 1));
+                    res[row][col] = curh + 1;
                 }
             }
         }
