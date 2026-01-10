263704ac2f52401004d298ea7c590a57146195e9
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-10 19:04:01 +0530

    Time: 134 ms (37.93%), Space: 287.6 MB (32.76%) - LeetHub

--- 3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
+++ 3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3373-maximize-the-number-of-target-nodes-after-connecting-trees-ii.java
@@ -6,12 +6,12 @@ class Solution {
             a[node] = col;
             count[col]++;
         } else {
-            int col=0;
+            int col = 0;
             a[node] = col;
             count[col]++;
         }
-        for (int k : adj.get(node)){
-            if (a[k]==-1){
+        for (int k : adj.get(node)) {
+            if (a[k] == -1) {
                 dfs(adj, a, count, k, !color);
             }
         }
@@ -40,24 +40,24 @@ class Solution {
         int color2[] = new int[m];
         Arrays.fill(color1, -1);
         Arrays.fill(color2, -1);
-        int count1[]=new int[2];
-        int count2[]=new int[2];
-        dfs(adj1, color1, count1, 0, true); 
+        int count1[] = new int[2];
+        int count2[] = new int[2];
+        dfs(adj1, color1, count1, 0, true);
         dfs(adj2, color2, count2, 0, true);
-        int nodes1[]=new int[n];
-        int nodes2[]=new int[m];
-        for(int i=0;i<n;i++){
-            nodes1[i]=count1[color1[i]];
+        int nodes1[] = new int[n];
+        int nodes2[] = new int[m];
+        for (int i = 0; i < n; i++) {
+            nodes1[i] = count1[color1[i]];
         }
-        for(int i=0;i<m;i++){
-            nodes2[i]=count2[color2[i]];
+        for (int i = 0; i < m; i++) {
+            nodes2[i] = count2[color2[i]];
         }
-        int max2=-1;
-        for(int i=0;i<m;i++){
-            max2=Math.max(max2, nodes2[i]);
+        int max2 = -1;
+        for (int i = 0; i < m; i++) {
+            max2 = Math.max(max2, nodes2[i]);
         }
-        for(int i=0;i<n;i++){
-            nodes1[i]=nodes1[i]+max2;
+        for (int i = 0; i < n; i++) {
+            nodes1[i] = nodes1[i] + max2;
         }
         return nodes1;
     }
