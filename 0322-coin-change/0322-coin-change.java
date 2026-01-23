1468e67c2c27a05c6d06e2483a9b55b5817794f4
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-23 22:50:38 +0530

    Time: 15 ms (81.89%), Space: 47.1 MB (28.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,24 @@
 class Solution {
-    private int cal(int a[], int val, int i, int dp[][]) {
-        if (i == 0) {
-            if (val % a[0] == 0) {
-                return val / a[0];
-            } else {
-                return 1000000000;
-            }
-        }
-        if (dp[i][val] != -1) {
-            return dp[i][val];
-        }
-        int nottake = 0 + cal(a, val, i - 1, dp);
-        int take = Integer.MAX_VALUE;
-        if (a[i] <= val) {
-            take = 1 + cal(a, val - a[i], i, dp);
-        }
-        return dp[i][val] = Math.min(take, nottake);
-    }
-
     public int coinChange(int[] coins, int amount) {
         int dp[][] = new int[coins.length][amount + 1];
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            } else {
+                dp[0][i] = 1000000000;
             }
         }
-        int res = cal(coins, amount, coins.length - 1, dp);
+        for (int i = 1; i < coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int nottake = 0 + dp[i-1][j];
+                int take = Integer.MAX_VALUE;
+                if (coins[i] <= j) {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                dp[i][j] = Math.min(take, nottake);
+            }
+        }
+        int res = dp[coins.length - 1][amount];
         return res >= 1000000000 ? -1 : res;
     }
 }
