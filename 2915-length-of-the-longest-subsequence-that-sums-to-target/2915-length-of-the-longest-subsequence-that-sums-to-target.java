2e82cf8ad0380892b9b5bd5dbda1698f3d93a8a5
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-25 14:46:56 +0530

    Time: 204 ms (59.53%), Space: 50.9 MB (42.23%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,27 +1,26 @@
 class Solution {
-    private int cal(List<Integer> num, int i, int target, int dp[][]) {
-        if (target == 0) {
-            return 0;
-        }
-        if (i > num.size() - 1 || target < 0) {
-            return -1000000000;
-        }
-        if (dp[i][target] != -1) {
-            return dp[i][target];
-        }
-        int notpick = cal(num, i + 1, target, dp);
-        int pick = 1 + cal(num, i + 1, target - num.get(i), dp);
-        return dp[i][target] = Math.max(notpick, pick);
-    }
-
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
         int dp[][] = new int[nums.size()][target + 1];
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j <= target; j++) {
-                dp[i][j] = -1;
+            dp[i][0] = 0;
+        }
+        for (int i = 1; i <= target; i++) {
+            if(nums.get(nums.size()-1)==i){
+                dp[nums.size()-1][i]=1;
+            } else {
+                dp[nums.size()-1][i]=-1000000000;
             }
         }
-        int res = cal(nums, 0, target, dp);
-        return (res < 0) ? -1 : res;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            for (int j = 0; j <= target; j++) {
+                int notpick = dp[i + 1][j];
+                int pick = -1000000000;
+                if (j >= nums.get(i)) {
+                    pick = 1 + dp[i + 1][j - nums.get(i)];
+                }
+                dp[i][j] = Math.max(notpick, pick);
+            }
+        }
+        return (dp[0][target] < 0) ? -1 : dp[0][target];
     }
 }
