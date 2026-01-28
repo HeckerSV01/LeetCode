bcbac669a845eb1888a3b9f7a4972491536ceecf
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-28 14:02:13 +0530

    Time: 5 ms (97.56%), Space: 140.6 MB (71.08%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,23 +1,20 @@
 class Solution {
-    private long cal(int a[][], int i, long dp[]) {
-        if (i >= a.length) {
-            return 0;
-        }
-        if (i == a.length - 1) {
-            return a[a.length - 1][0];
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        long skip = cal(a, i + 1, dp);
-        long notskip = a[i][0] + cal(a, i + a[i][1] + 1, dp);
-        return dp[i] = Math.max(skip, notskip);
-    }
-
     public long mostPoints(int[][] questions) {
         int m = questions.length;
-        long dp[] = new long[m];
-        Arrays.fill(dp, -1);
-        return cal(questions, 0, dp);
+        long dp[] = new long[m+1];
+        dp[m-1]=(long)questions[m-1][0];
+        for(int i=m-1;i>=0;i--){
+            long skip=dp[i+1];
+            int nxt=i+questions[i][1]+1;
+            long k=0;
+            if(nxt>m){
+                k=0;
+            }else{
+                k=dp[nxt];
+            }
+            long notskip=questions[i][0]+k;
+            dp[i]=Math.max(skip,notskip);
+        }
+        return dp[0];
     }
 }
