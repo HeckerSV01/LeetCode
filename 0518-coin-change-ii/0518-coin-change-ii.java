579747532a766667ee430c5b661ba74e8fc10a17
Sumedh Verma <SV839030@GMAIL.COM>
2026-01-30 21:20:00 +0530

    Time: 10 ms (68.33%), Space: 57.7 MB (9.44%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,21 @@
 class Solution {
-    private int cal(int a[], int i, int target, int dp[][]) {
-        if (i == 0) {
-            return (target % a[0] == 0) ? 1 : 0;
-        }
-        if (dp[i][target] != -1) {
-            return dp[i][target];
-        }
-        int notselect = cal(a, i - 1, target, dp);
-        int select = 0;
-        if (a[i] <= target) {
-            select = cal(a, i, target - a[i], dp);
-        }
-        return dp[i][target] = notselect + select;
-    }
-
     public int change(int amount, int[] coins) {
         int dp[][] = new int[coins.length][amount + 1];
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                dp[i][j] = -1;
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = 1;
             }
         }
-        return cal(coins, coins.length - 1, amount, dp);
+        for (int i = 1; i < coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int notselect = dp[i - 1][j];
+                int select = 0;
+                if (coins[i] <= j) {
+                    select = dp[i][j - coins[i]];
+                }
+                dp[i][j] = select + notselect;
+            }
+        }
+        return dp[coins.length - 1][amount];
     }
 }
