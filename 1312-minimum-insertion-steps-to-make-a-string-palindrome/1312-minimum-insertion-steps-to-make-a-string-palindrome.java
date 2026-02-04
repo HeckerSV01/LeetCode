9b8424e2f6e43327803b9ee542167b56d6cbed9c
Sumedh Verma <SV839030@GMAIL.COM>
2026-02-04 18:56:50 +0530

    Time: 36 ms (15.01%), Space: 47.4 MB (60.97%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int lcs(String text1, String text2) {
-        int dp[][] = new int[text1.length() + 1][text2.length() + 1];
-        for (int i = 1; i < text1.length() + 1; i++) {
-            for (int j = 1; j < text2.length() + 1; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+    public int lcs(String s1,String s2,int i1,int i2,int dp[][]) {
+        if(i1<0||i2<0){
+            return 0;
         }
-        return dp[text1.length()][text2.length()];
+        if(dp[i1][i2]!=-1){
+            return dp[i1][i2];
+        }
+        if(s1.charAt(i1)==s2.charAt(i2)){
+            return 1+lcs(s1,s2,i1-1,i2-1,dp);
+        }
+        return dp[i1][i2]=Math.max(lcs(s1,s2,i1-1,i2,dp),lcs(s1,s2,i1,i2-1,dp));
     }
 
     public int minInsertions(String s) {
-        return s.length() - lcs(s, new StringBuilder(s).reverse().toString());
+        int dp[][]=new int[s.length()][s.length()];
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<s.length();j++){
+                dp[i][j]=-1;
+            }
+        }
+        return s.length() - lcs(s, new StringBuilder(s).reverse().toString(),s.length()-1,s.length()-1,dp);
     }
 }
