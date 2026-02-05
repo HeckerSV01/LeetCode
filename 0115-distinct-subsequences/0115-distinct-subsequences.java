3a321c1fda34529f7303834cefdff9869c804696
Sumedh Verma <SV839030@GMAIL.COM>
2026-02-05 23:59:54 +0530

    Time: 22 ms (26.15%), Space: 54.2 MB (71.4%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,29 +1,21 @@
 class Solution {
-    private int cal(String s1, String s2, int i, int j, int dp[][]){
-        if(j<0){
-            return 1;
-        }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s1.charAt(i)==s2.charAt(j)){
-            int take=cal(s1,s2,i-1,j-1,dp);
-            int nottake=cal(s1,s2,i-1,j,dp);
-            return dp[i][j]=take+nottake;
-        } else {
-            return dp[i][j]=cal(s1,s2,i-1,j,dp);
-        }
-    }
+
     public int numDistinct(String s, String t) {
-        int dp[][]=new int[s.length()][t.length()];
-        for(int i=0;i<s.length();i++){
-            for(int j=0;j<t.length();j++){
-                dp[i][j]=-1;
+        int dp[][] = new int[s.length() + 1][t.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    int take = dp[i - 1][j - 1];
+                    int nottake = dp[i - 1][j];
+                    dp[i][j] = take + nottake;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        return cal(s,t,s.length()-1,t.length()-1,dp);
+        return dp[s.length()][t.length()];
     }
 }
