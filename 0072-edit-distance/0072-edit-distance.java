41f2a066485a764ecc511ace7e11a15e33d8121b
Sumedh Verma <SV839030@GMAIL.COM>
2026-02-07 17:26:42 +0530

    Time: 6 ms (10.51%), Space: 47.4 MB (30.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,31 +1,25 @@
 class Solution {
-    private int cal(String s1, String s2, int i, int j, int dp[][]) {
-        if (i < 0) {
-            return j + 1;
-        }
-        if (j < 0) {
-            return i + 1;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return dp[i][j] = cal(s1, s2, i - 1, j - 1, dp);
-        } else {
-            int del = 1 + cal(s1, s2, i - 1, j, dp);
-            int ins = 1 + cal(s1, s2, i, j - 1, dp);
-            int rep = 1 + cal(s1, s2, i - 1, j - 1, dp);
-            return dp[i][j] = Math.min(del, Math.min(ins, rep));
-        }
-    }
 
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-        for (int i = 0; i < word1.length(); i++) {
-            for (int j = 0; j < word2.length(); j++) {
-                dp[i][j] = -1;
+        int dp[][] = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int del = 1 + dp[i - 1][j];
+                    int ins = 1 + dp[i][j - 1];
+                    int rep = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = Math.min(del, Math.min(ins, rep));
+                }
             }
         }
-        return cal(word1, word2, word1.length() - 1, word2.length() - 1, dp);
+        return dp[word1.length()][word2.length()];
     }
 }
