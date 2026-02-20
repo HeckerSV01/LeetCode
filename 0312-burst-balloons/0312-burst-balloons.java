edfc72a23a13425a8129538b5501c943ce6ec0e3
Sumedh Verma <SV839030@GMAIL.COM>
2026-02-20 14:25:11 +0530

    Time: 41 ms (88.36%), Space: 44.3 MB (82.8%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,19 +1,4 @@
 class Solution {
-    private int cal(int a[], int i, int j, int dp[][]) {
-        if (i > j) {
-            return 0;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int max = -1;
-        for (int k = i; k <= j; k++) {
-            int cost = a[i - 1] * a[j + 1] * a[k] + cal(a, i, k - 1, dp) + cal(a, k + 1, j, dp);
-            max = Math.max(max, cost);
-        }
-        return dp[i][j] = max;
-    }
-
     public int maxCoins(int[] nums) {
         int a[] = new int[nums.length + 2];
         a[0] = 1;
@@ -22,11 +7,19 @@ class Solution {
             a[i + 1] = nums[i];
         }
         int dp[][] = new int[a.length][a.length];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                dp[i][j] = -1;
+        for (int i = a.length - 1; i >= 1; i--) {
+            for (int j = 1; j < a.length - 1; j++) {
+                if (i > j) {
+                    continue;
+                }
+                int max = -1;
+                for (int k = i; k <= j; k++) {
+                    int cost = a[i - 1] * a[j + 1] * a[k] + dp[i][k - 1] + dp[k + 1][j];
+                    max = Math.max(max, cost);
+                }
+                dp[i][j] = max;
             }
         }
-        return cal(a, 1, a.length - 2, dp);
+        return dp[1][a.length - 2];
     }
 }
