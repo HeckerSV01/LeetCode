3b887888647da702297eafc40f802fcb9cc7fe9a
Sumedh Verma <SV839030@GMAIL.COM>
2026-02-26 00:43:04 +0530

    Time: 1 ms (35.24%), Space: 42.4 MB (11.09%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int cal(long n, HashMap<Long,Integer> dp) {
+        if (n == 1) {
+            return 0;
+        }
+        if(dp.containsKey(n)){
+            return dp.get(n);
+        }
+        int op1 = Integer.MAX_VALUE;
+        int op2 = Integer.MAX_VALUE;
+        int op3 = Integer.MAX_VALUE;
+        if (n % 2 == 0) {
+            op1 = 1 + cal(n / 2, dp);
+        } else {
+            op2 = 1 + cal(n + 1, dp);
+            op3 = 1 + cal(n - 1, dp);
+        }
+        int res=Math.min(op1,Math.min(op2,op3));
+        dp.put(n,res);
+        return res;
+    }
+
+    public int integerReplacement(int n) {
+        HashMap<Long,Integer> dp=new HashMap<>();
+        return cal((long)n, dp);
+    }
+}
