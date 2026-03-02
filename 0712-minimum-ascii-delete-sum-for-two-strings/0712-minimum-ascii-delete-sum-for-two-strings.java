18ea831d034a4af3488222ef9e0a48c100c69ddd
Sumedh Verma <SV839030@GMAIL.COM>
2026-03-02 20:34:22 +0530

    Time: 17 ms (59.28%), Space: 46.3 MB (88.53%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -4,7 +4,7 @@ class Solution {
         for (int i = 1; i < s1.length() + 1; i++) {
             for (int j = 1; j < s2.length() + 1; j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[i][j] = s1.charAt(i-1) + dp[i - 1][j - 1];
+                    dp[i][j] = s1.charAt(i - 1) + dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
@@ -12,16 +12,17 @@ class Solution {
         }
         return dp[s1.length()][s2.length()];
     }
+
     public int minimumDeleteSum(String s1, String s2) {
-        int sum1=0;
-        int sum2=0;
-        for(int i=0;i<s1.length();i++){
-            sum1=sum1+(s1.charAt(i));
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            sum1 = sum1 + (s1.charAt(i));
         }
-        for(int i=0;i<s2.length();i++){
-            sum2=sum2+(s2.charAt(i));
+        for (int i = 0; i < s2.length(); i++) {
+            sum2 = sum2 + (s2.charAt(i));
         }
-        int k=lcs(s1,s2);
-        return sum1-k+sum2-k;
+        int k = lcs(s1, s2);
+        return sum1 - k + sum2 - k;
     }
 }
