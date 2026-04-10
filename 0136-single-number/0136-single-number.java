047db25c96e02d3a0e5da9ad679308ab4bcfc6fa
Sumedh Verma <SV839030@GMAIL.COM>
2026-04-10 17:59:00 +0530

    Time: 1 ms (99.98%), Space: 46.6 MB (96.57%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int k:nums){
-            map.put(k,map.getOrDefault(k,0)+1);
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            res=res^nums[i];
         }
-        for(int k:map.keySet()){
-            if(map.get(k)==1){
-                return k;
-            }
-        }
-        return -1;
+        return res;
     }
 }
