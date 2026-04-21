9662a0c4bf493c72d7af6befe41f9c3a45f8ea0f
Sumedh Verma <SV839030@GMAIL.COM>
2026-04-21 22:56:51 +0530

    Time: 2 ms (99.27%), Space: 45.2 MB (51.06%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,34 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       Stack<Integer> s=new Stack<>();
-       int temp[]=new int[nums2.length];
-       int res[]=new int[nums1.length];
-       for(int i=nums2.length-1;i>=0;i--)
-       {
-         while(!s.isEmpty()&&nums2[s.peek()]<=nums2[i])
-         {
-            s.pop();
-         }
-         if(s.isEmpty())
-         {
-            temp[i]=-1;
-         }
-         else
-         {
-            temp[i]=nums2[s.peek()];
-         }
-         s.push(i);
-        } 
-        for(int i=0;i<nums1.length;i++)
-        {
-            for(int j=0;j<nums2.length;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    res[i]=temp[j];
-                    break;
-                }
+        int temp[]=new int[10001];
+        Stack<Integer> st=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty()&&nums2[i]>=st.peek()){
+                st.pop();
             }
+            if(st.isEmpty()){
+                temp[nums2[i]]=-1;
+            }else{
+                temp[nums2[i]]=st.peek();
+            }
+            st.push(nums2[i]);
+        }
+        int res[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i]=temp[nums1[i]];
         }
         return res;
     }
