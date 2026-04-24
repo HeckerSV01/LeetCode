a7abf853e73d3acd9f307fddb5564a06799ff61c
Sumedh Verma <SV839030@GMAIL.COM>
2026-04-25 02:02:56 +0530

    Time: 11 ms (5.27%), Space: 45.5 MB (84.21%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='U'){
+                a++;
+            }
+            if(moves.charAt(i)=='D'){
+                a--;
+            }
+            if(moves.charAt(i)=='R'){
+                b++;
+            }
+            if(moves.charAt(i)=='L'){
+                b--;
+            }
+        }
+        return (a==0)&&(b==0);
+    }
+}
