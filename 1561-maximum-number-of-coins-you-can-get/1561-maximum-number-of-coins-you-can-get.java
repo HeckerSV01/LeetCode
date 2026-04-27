82fd08c71b249daf56e817994147c3f5d10f21b8
Sumedh Verma <SV839030@GMAIL.COM>
2026-04-28 01:37:58 +0530

    Time: 31 ms (64.41%), Space: 69.2 MB (58.41%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int res=0;
+        for(int i=piles.length-2;i>=piles.length/3;i=i-2){
+            res=res+piles[i];
+        }
+        return res;
+    }
+}
