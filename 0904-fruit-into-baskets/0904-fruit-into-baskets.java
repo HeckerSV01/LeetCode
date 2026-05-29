967ae1415d9253d4f4195e873891752665a19b7d
Sumedh Verma <SV839030@GMAIL.COM>
2026-05-29 11:35:00 +0530

    Time: 50 ms (86.37%), Space: 67.3 MB (91.68%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -7,7 +7,7 @@ class Solution {
         int n=fruits.length;
         while(r<n){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
-            while(map.size()>2){
+            if(map.size()>2){
                 int k=fruits[l];
                 if(map.get(k)>1){
                     map.put(k,map.get(k)-1);
