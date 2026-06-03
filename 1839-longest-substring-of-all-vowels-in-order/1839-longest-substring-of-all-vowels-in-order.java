ce119a97ad930a24993978a50cb38ac38e09b98d
Sumedh Verma <SV839030@GMAIL.COM>
2026-06-03 14:11:20 +0530

    Time: 30 ms (50.34%), Space: 50.8 MB (70.81%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.java
@@ -3,19 +3,19 @@ class Solution {
         int l=0;
         int r=1;
         int res=0;
-        HashSet<Character> set=new HashSet<>();
-        set.add(word.charAt(0));
+        int unique=1;
         while(r<word.length()){
             if(word.charAt(r-1)<=word.charAt(r)){
-                set.add(word.charAt(r));
-                if(set.size()==5){
+                if(word.charAt(r-1)<word.charAt(r)){
+                    unique++;
+                }
+                if(unique==5){
                     res=Math.max(res,r-l+1);
                 }
                 r++;
             }else{
-                set.clear();
                 l=r;
-                set.add(word.charAt(l));
+                unique=1;
                 r++;
             }
         }
