3db2785d65f726e569d64f9407882daabc3a67cb
Sumedh Verma <SV839030@GMAIL.COM>
2026-06-12 23:06:24 +0530

    Time: 67 ms (6.29%), Space: 99 MB (5.15%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,13 +1,23 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int count=0;
-        int i=0;
-        while(coins>0&&i<costs.length&&costs[i]<=coins){
-            coins-=costs[i];
-            count++;
-            i++;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int max=-1;
+        for(int i=0;i<costs.length;i++){
+            map.put(costs[i],map.getOrDefault(costs[i],0)+1);
+            max=Math.max(max,costs[i]);
         }
-        return count;
+        int res=0;
+        for(int i=1;i<=max&&coins>0;i++){
+            if(!map.containsKey(i)){
+                continue;
+            }
+            if(map.containsKey(i)&&map.get(i)==0){
+                continue;
+            }
+            int k=Math.min(map.get(i),coins/i);
+            res=res+k;
+            coins=coins-k*i;
+        }
+        return res;
     }
 }
