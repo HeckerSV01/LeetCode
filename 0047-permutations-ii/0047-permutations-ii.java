38a66943da0a747925f8aa43ede1883fd06c182a
Sumedh Verma <SV839030@GMAIL.COM>
2026-06-16 11:01:42 +0530

    Time: 4 ms (28.9%), Space: 46.4 MB (94.2%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,31 +1,27 @@
 class Solution {
-    public void backtrack(int a[],List<Integer> list,HashMap<Integer,Integer> map,HashSet<ArrayList<Integer>> set){
+    public void backtrack(int a[],List<Integer> list,HashMap<Integer,Integer> map,List<List<Integer>> res){
         if(list.size()==a.length){
-            set.add(new ArrayList<>(list));
+            res.add(new ArrayList<>(list));
             return;
         }
-        for(int i=0;i<a.length;i++){
-            if(map.get(a[i])>0){
-                map.put(a[i],map.get(a[i])-1);
-                list.add(a[i]);
-                backtrack(a,list,map,set);
+        for(int k:map.keySet()){
+            if(map.get(k)>0){
+                map.put(k,map.get(k)-1);
+                list.add(k);
+                backtrack(a,list,map,res);
                 list.remove(list.size()-1);
-                map.put(a[i],map.get(a[i])+1);
+                map.put(k,map.get(k)+1);
             }
         }
     }
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
-        HashSet<ArrayList<Integer>> set=new HashSet<>();
         List<Integer> list=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        backtrack(nums,list,map,set);
-        for(List<Integer> k:set){
-            res.add(k);
-        }
+        backtrack(nums,list,map,res);
         return res;
     }
 }
