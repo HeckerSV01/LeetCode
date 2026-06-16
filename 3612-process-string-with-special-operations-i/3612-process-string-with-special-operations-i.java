07d45890881980cd122859f346c44af7a9e44b7c
Sumedh Verma <SV839030@GMAIL.COM>
2026-06-16 11:57:45 +0530

    Time: 4 ms (76.47%), Space: 55.5 MB (36.76%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c>='a'&&c<='z'){
+                sb.append(c);
+            }else if(c=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }else if(c=='#'){
+                String str=sb.toString();
+                sb.append(str);
+            }else{
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
