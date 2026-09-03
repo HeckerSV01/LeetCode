951282bc8d66950d1bce65c64499da5bf3d12aec
Sumedh Verma <SV839030@GMAIL.COM>
2026-09-03 13:04:23 +0530

    Time: 2 ms (97.94%), Space: 72.1 MB (93.55%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean lemonadeChange(int[] nums) {
+        int count5=0;
+        int count10=0;
+        int count20=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==5){
+                count5++;
+            }else if(nums[i]==10){
+                count10++;
+                if(count5>=1){
+                    count5--;
+                }else{
+                    return false;
+                }
+            }else{
+                count20++;
+                if(count10<3&&count5>=3){
+                    count5=count5-3;
+                }else if(count10>=1&&count5>=1){
+                    count10--;
+                    count5--;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
