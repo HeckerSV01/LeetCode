15c2401974bc3fb64af043131bf6f7fb98774fac
Sumedh Verma <SV839030@GMAIL.COM>
2026-09-10 12:10:39 +0530

    Time: 2 ms (96.92%), Space: 43.3 MB (77.69%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -1,23 +1,13 @@
 class Solution {
-    private int solve(int arr[][], int i, int a, int b,Integer dp[][][]){
-        if(i==arr.length){
-            return 0;
-        }
-        if(dp[i][a][b]!=null){
-            return dp[i][a][b];
-        }
-        int take1=Integer.MAX_VALUE;
-        int take2=Integer.MAX_VALUE;
-        if(a>0){
-            take1=Math.min(take1, arr[i][0]+solve(arr,i+1,a-1,b,dp));
-        }
-        if(b>0){
-            take2=Math.min(take2, arr[i][1]+solve(arr,i+1,a,b-1,dp));
-        }
-        return dp[i][a][b]=Math.min(take1,take2);
-    }
     public int twoCitySchedCost(int[][] costs) {
-        Integer dp[][][]=new Integer[costs.length][51][51];
-        return solve(costs,0,costs.length/2,costs.length/2,dp);
+        int suma=0;
+        for(int k[]:costs){
+            suma=suma+k[0];
+        }
+        Arrays.sort(costs,(a,b)->a[1]-a[0]-(b[1]-b[0]));
+        for(int i=0;i<costs.length/2;i++){
+            suma=suma-costs[i][0]+costs[i][1];
+        }
+        return suma;
     }
 }
